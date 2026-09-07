086d01338c11f399583079cdf878292ae3bdd110
li-jinggg <koahlijing178@gmail.com>
2026-09-07 22:53:21 +0800

    Update main.tsx for routing and component structure
    Refactor main.tsx to use ReactDOM and add TestPage route.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
-  import { createRoot } from "react-dom/client";
-  import { BrowserRouter } from "react-router-dom"; // 1. Import BrowserRouter
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+import React from 'react'
+import ReactDOM from 'react-[#1B2D4F]-dom/client'
+import App from './App.tsx'
+import TestPage from './TestPage.jsx'
+import './index.css'
 
+const isTestRoute = window.location.pathname === '/test';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {isTestRoute ? <TestPage /> : <App />}
+  </React.StrictMode>,
+)
   createRoot(document.getElementById("root")!).render(
     <BrowserRouter> {/* 2. Wrap App inside BrowserRouter */}
     <App />
