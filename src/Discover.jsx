3f459f5eb18f2804a374492fa53466209d239575
li-jinggg <koahlijing178@gmail.com>
2026-09-07 22:31:55 +0800

    Update import paths and add Navbar component

--- src/Discover.jsx
+++ src/Discover.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FoodPage from "./pages/FoodPage";
-import StallPage from "./pages/StallPage";
+import FoodPage from "../FoodPage";
+import StallPage from "../StallPage";
+import Navbar from "../components/navigation-menu"; // Adjust path if navigation-menu is in a different location
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        {/* Home / Default Page */}
         <Route path="/" element={<FoodPage />} />
+
+        {/* Discover Page linked from your Navbar */}
+        <Route path="/discover" element={<FoodPage />} />
+
+        {/* Individual Stall Details Page */}
         <Route path="/stall/:id" element={<StallPage />} />
+
+        {/* Catch-all fallback */}
+        <Route path="*" element={<FoodPage />} />
       </Routes>
     </BrowserRouter>
   );
