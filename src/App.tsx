f87195e48fe106a4df0710a19028d14b08ef5a30
li-jinggg <koahlijing178@gmail.com>
2026-09-07 23:01:21 +0800

    Add App component with conditional rendering

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  if (window.location.pathname === '/test') {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', margin: 0, fontSize: '48px', fontWeight: 'bold' }}>
+        DICK
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {/* Main App Code */}
+    </div>
+  );
+}
