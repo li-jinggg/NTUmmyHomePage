5c3a8af4b539f711ae50a9a6ad7dd77d107a9f5f
li-jinggg <koahlijing178@gmail.com>
2026-09-07 19:57:02 +0800

    Add BrowserRouter to main.tsx
    Wrap App component with BrowserRouter for routing support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-
   import { createRoot } from "react-dom/client";
+  import { BrowserRouter } from "react-router-dom"; // 1. Import BrowserRouter
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <BrowserRouter> {/* 2. Wrap App inside BrowserRouter */}
+    <App />
+  </BrowserRouter>
+);
   
